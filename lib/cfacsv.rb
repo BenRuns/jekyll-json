8335e8c9353b1ee27e4d86011eda1c28ccfdd2a6
BenRuns <benthomaspatterson@gmail.com>
2015-04-03 19:04:55 -0700

    refactor

--- lib/cfacsv.rb
+++ lib/cfacsv.rb
@@ -11,17 +11,21 @@ module Jekyll
 
         def generate(site)
             
-            data_files = YAML.load_file(File.join(site.source, '_data/projects.yml'))
-            header = %w[name description link_url code_url type categories status]
+            yml_file = YAML.load_file(File.join(site.source, '_data/projects.yml'))
+            headers = %w[name description link_url code_url type categories status]
+            path_to_destination = File.join(site.source,"projects/index.csv")
+            convert_yaml_to_csv(yml_file, path_to_destination, headers)
+        end
 
-            CSV.open(File.join(site.source,"projects.csv"), "wb") do |csv|
-              csv << header
+        def convert_yaml_to_csv(yml_file, path_to_destination, headers)
+            CSV.open(path_to_destination, "wb") do |csv|
+              csv << headers
               data_files.each do |data|
                 row = []
-                header.each { |header| data[header] ? row << data[header] : row << nil }
+                headers.each { |header| data[header] ? row << data[header] : row << nil }
                 csv << row
               end
             end
-        end
+        end    
     end
 end
